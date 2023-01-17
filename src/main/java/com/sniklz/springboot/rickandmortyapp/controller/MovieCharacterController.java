4e09a491d523b2ff6d4c385a1f407d04b8ece5e4
Igor Bukur <tuturox91@gmail.com>
2023-01-17 09:13:18 +0200

    Some fixes and changes

--- src/main/java/com/sniklz/springboot/rickandmortyapp/controller/MovieCharacterController.java
+++ src/main/java/com/sniklz/springboot/rickandmortyapp/controller/MovieCharacterController.java
@@ -3,6 +3,8 @@ package com.sniklz.springboot.rickandmortyapp.controller;
 import com.sniklz.springboot.rickandmortyapp.dto.CharacterResponseDto;
 import com.sniklz.springboot.rickandmortyapp.dto.mapper.MovieCharacterMapper;
 import com.sniklz.springboot.rickandmortyapp.service.MovieCharacterService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,12 +26,16 @@ public class MovieCharacterController {
     }
 
     @GetMapping("/random")
+    @ApiOperation("Get random movie character")
     public CharacterResponseDto getRandom() {
         return mapper.toResponseDto(characterService.getRandomCharacter());
     }
 
     @GetMapping("/by-name")
-    public List<CharacterResponseDto> findAllByName(@RequestParam("name") String namePart) {
+    @ApiOperation("Get all movie characters with a query by their name")
+    public List<CharacterResponseDto> findAllByName(
+            @RequestParam("name")
+            @ApiParam(value = "Name or part of name") String namePart) {
         return characterService.findAllByNameContains(namePart)
                 .stream()
                 .map(mapper::toResponseDto)
